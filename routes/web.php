d4c7be811bf3059434f9ac422b121e7550188a54
mohamed-elhauari <elhauari.imohamed@etu.uae.ac.ma>
2024-12-17 13:37:42 +0100

    correction routes web.php

--- routes/web.php
+++ routes/web.php
@@ -10,58 +10,6 @@ use App\Http\Controllers\EtudiantController;
 use App\Http\Controllers\CustomAdminController;
 use App\Http\Controllers\ReclamationController;
 
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/welcome', function () {
-    return Inertia::render('Etudiant/welcome');
-})->name('welcome');
-
-Route::get('/demande', function () {
-    return Inertia::render('Etudiant/demande_document');
-})->name('demande_document');
-
-Route::get('/reclamation', function () {
-    return Inertia::render('Etudiant/reclamation');
-})->name('reclamation');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Admin/dashboard');
-})->name('dashboard');
-
-Route::get('/contacter-nous', function () {
-    return Inertia::render('Etudiant/contacter');
-})->name('contacter');
-
-Route::get('/demande-releve-notes', function () {
-    return Inertia::render('Etudiant/type_demande/DemandeReleveNote');
-})->name('DemandeReleveNote');
-
-Route::get('/demande-convention', function () {
-    return Inertia::render('Etudiant/type_demande/demandeconvention');
-})->name('DemandeConvention');
-
-Route::get('/demande-attestation', function () {
-    return Inertia::render('Etudiant/type_demande/DemandeAttestationReussite');
-})->name('DemandeAttestation');
-
-
-
-//admin
-
-Route::get('/reclamations_admin', [ReclamationController::class, 'showReclamations'])->name('reclamationadmin');;
-Route::get('/probleme_technique/{id}', [ReclamationController::class, 'showProblemeTechnique'])->name('probleme_technique');
-Route::get('/accepter_attestation_reuissite/{id}', [ReclamationController::class, 'attestationreuissitePDF'])->name('attestationreuissitePDF');
-
-//fin
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -69,8 +17,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-
 
 Route::get('/', function () {
     return to_route('welcome');
@@ -105,7 +51,7 @@ Route::get('/demande-details-releve-notes/{id}', [CustomAdminController::class,'
 Route::get('/accepter_convention_stage/{id}', [CustomAdminController::class,'accepter_demande_convention'])->name('accepter.demande.convention');
 Route::get('/accepter_attestation_scolarite/{id}', [CustomAdminController::class,'accepter_attestation_scolarite'])->name('accepter.demande.attestation.scolarite');
 Route::get('/accepter_relevee_notes/{id}', [CustomAdminController::class,'accepter_relevee_notes'])->name('accepter.demande.relevee_notes');
-
+Route::get('/accepter_attestation_reuissite/{id}', [ReclamationController::class, 'attestationreuissitePDF'])->name('attestationreuissitePDF');
 
 
 
